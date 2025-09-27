1148d5e0d85f29b3136e5996514d79a61b95e3e5
Vadapalli Vivek <117800701+vadapallivivek@users.noreply.github.com>
2025-09-27 18:20:42 +0530

    Create trivy.sh

---
+++ trivy.sh
@@ -0,0 +1,10 @@
+echo 'export PATH=$PATH:/usr/local/bin/' >> ~/.bashrc
+source .bashrc
+
+
+
+
+wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.tar.gz
+tar zxvf trivy_0.18.3_Linux-64bit.tar.gz
+sudo mv trivy /usr/local/bin/
+trivy --version
